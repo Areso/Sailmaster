ae1842f3f9757fc30f6201eaa262a265dded7970
Anton Gladyshev <returnstrike@yandex.ru>
2020-11-01 02:20:13 +0300

    changed address of host machine

---
+++ webserver/k6-tests/docker.js
@@ -0,0 +1,12 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = {
+  vus: 3,
+  duration: "10s"
+};
+
+export default function() {
+  const res = http.get("http://172.17.0.1:6689/api/v1.0/heartbeat");
+  check(res, { "status was 200": r => r.status == 200 })
+};

8ce56f1af35b746d0a95dce304b8fd6f5d52561e
Areso <returnstrike@yandex.ru>
2020-11-17 00:10:36 +0300

    automate user creation

--- database/1_users.sql
+++ database/1_users.sql
@@ -1,3 +1,6 @@
-CREATE USER 'sailmaster-user'@'localhost' IDENTIFIED BY 'TYPE_DB_USER_PASSWORD_HERE';
-GRANT USAGE ON *.* TO 'sailmaster-user'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-GRANT ALL PRIVILEGES ON `sailmaster`.* TO 'sailmaster-user'@'localhost';
+USE sailmaster;
+CREATE USER IF NOT EXISTS 'sailmaster_user'@'localhost' IDENTIFIED BY 'password';
+--MySQL 5.7.6 or less
+--GRANT ALL ON `sailmaster`.* TO 'sailmaster_user'@'localhost' IDENTIFIED BY 'password';
+GRANT USAGE ON *.* TO 'sailmaster_user'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_P$
+GRANT ALL PRIVILEGES ON `sailmaster`.* TO 'sailmaster_user'@'localhost';
